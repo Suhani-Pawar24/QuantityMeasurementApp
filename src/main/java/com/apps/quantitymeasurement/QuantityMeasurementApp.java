cd84ce4bf4c711b838b736e1364117009a472cc9
PSuhani2404 <suhani.pawar04@gmail.com>
2026-02-23 16:17:09 +0530

    [Suhani] : added addition of two length units

--- src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -2,6 +2,7 @@ package com.apps.quantitymeasurement;
 
 public class QuantityMeasurementApp {
 
+
 		// Equality demo
 		public static boolean demonstrateLengthEquality(Length l1, Length l2) {
 			return l1.equals(l2);
